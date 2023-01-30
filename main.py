dde5bb94e191e6eff1164e58153ede122b9f3477
Eduardo Rosa <92902577+RosaMaster@users.noreply.github.com>
2023-01-30 00:27:20 -0300

    Add file in repo
    Add flie main.py and requirements.txt

---
+++ main.py
@@ -0,0 +1,15 @@
+# Cotação de moedas
+import httpx
+
+base_currency = input("Digite a moeda de base: ").upper()
+currency = input("Digite a moeda desejada: ").upper()
+
+# BUSCA VALORES DE MOEDAS NA API PUBLICA
+response = httpx.get(
+    url=f'https://api.exchangerate.host/latest?base={base_currency}'
+).json()
+
+currency_data = response['rates']
+
+print(f'1 {base_currency} vale {currency_data.get(currency)} {currency}')
+
