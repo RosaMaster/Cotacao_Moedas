c927c04d3142d48546036b2704f82b89aaf9985e
Eduardo Rosa <92902577+RosaMaster@users.noreply.github.com>
2023-01-31 23:53:44 -0300

    inclusão lista consulta siglas moedas

---
+++ lista.py
@@ -0,0 +1,15 @@
+moedas = {
+    "USD": "Dólar dos EUA",
+    "EUR": "Euro",
+    "GBP": "Libra Esterlina",
+    "CAD": "Dólar Canadense",
+    "AUD": "Dólar Australiano",
+    "JPY": "Iene Japonês",
+    "RUB": "Rublo Russo",
+    "BRL": "Real Brasileiro",
+    "ARS": "Peso Argentino",
+    "CLP": "Peso Chileno",
+    "BOB": "Bolíviano da Bolívia",
+    "IDR": "Rupia Indonésia",
+    "CZK": "Coroa Checa",
+    }
